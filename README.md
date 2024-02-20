76cc46ee24ea2c2fd0f3828402b3da0d9bcbc159
Parker Martin <parkerjmartin883@gmail.com>
2024-02-19 21:48:57 -0700

    refactored to only ask prompt for how long the password should be

--- README.md
+++ README.md
@@ -7,9 +7,11 @@
 </p>
 ## Usage
 
-> Requirements file is empty, so there are no dependencies, will add some for potential database functionality later. run the command below. it will prompt you how many characters and what characters you want to have. as of now, all characters are required. just type y for all of the questions on what characters you want.
+> Requirements file is empty, so there are no dependencies, will add some for potential database functionality later. It will prompt you how many characters your password is.  The number can be between 8 and 20 (inclusive)
 > 
-> Run pythong
+> There is all 26 letters, lower and uppercase, all 10 digits (0-9) and most special characters available for use. Instead of prompting to ask which are available, just made it default with those.
+> 
+> Run the below command to execute the script.
 
 ```shell
 python3 main.py
--- main.py
+++ main.py
@@ -1,43 +1,34 @@
 import math
 import random
-import sys
 
 
 def write_password():
-    characters = int(input("how many characters do you want your password to be? Pick a number between 8 and 30"))
+    characters = int(input("how many characters do you want your password to be? Pick a number between 8 and 20"))
 
-    if characters < 8 or characters > 30:
-        return "please pick a number between 8 and 30"
+    if characters < 8 or characters > 20:
+        return "please pick a number between 8 and 20"
 
     lowercase = ["abcdefghijklmnopqrstuvwxyz"]
     uppercase = ["abcdefghijklmnopqrstuvwxyz".upper()]
     numbers = ["1234567890"]
     special = ["!@#$%^&*()?<>,./"]
-    has_lowercase = bool(input("want to include lowercase?"))
-    has_uppercase = bool(input("want to include uppercase?"))
-    has_numbers = bool(input("want to include numbers?"))
-    has_special_chars = bool(input("want to include special characters?"))
-    if not has_special_chars and not has_lowercase and not has_uppercase and not has_numbers:
-        print("make sure that you select criteria for a password, try again")
-        sys.exit(1)
+    # has_lowercase = bool(input("want to include lowercase?"))
+    # has_uppercase = bool(input("want to include uppercase?"))
+    # has_numbers = bool(input("want to include numbers?"))
+    # has_special_chars = bool(input("want to include special characters?"))
+    # if not has_special_chars and not has_lowercase and not has_uppercase and not has_numbers:
+    #     print("make sure that you select criteria for a password, try again")
+    #     sys.exit(1)
 
-    selected_criteria = []
-    if has_lowercase:
-        selected_criteria.append(*lowercase)
-    if has_uppercase:
-        selected_criteria.append(*uppercase)
-    if has_special_chars:
-        selected_criteria.append(*special)
-    if has_numbers:
-        selected_criteria.append(*numbers)
+    selected_criteria = [*lowercase, *uppercase, *special, *numbers]
 
     final_criteria = selected_criteria[0] + selected_criteria[1] + selected_criteria[2] + selected_criteria[3]
 
-    password = ""
+    pw_string = ""
     for i in range(characters):
-        password += final_criteria[math.floor(random.random() * len(final_criteria))]
+        pw_string += final_criteria[math.floor(random.random() * len(final_criteria))]
 
-    return password
+    return pw_string
 
 
 if __name__ == "__main__":
