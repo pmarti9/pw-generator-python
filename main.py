5369921d42cb77b4fe0c261fd95950c3bf4a3b7e
Parker Martin <parkerjmartin883@gmail.com>
2024-12-09 23:23:24 -0700

    updating to be a while loop

--- main.py
+++ main.py
@@ -2,29 +2,38 @@ import math
 import random
 
 
-def write_password():
-    characters = int(input("how many characters do you want your password to be? Pick a number between 8 and 20"))
+class Password:
+    def __init__(self, name):
+        self.name = name
 
-    if characters < 8 or characters > 20:
-        return "please pick a number between 8 and 20"
+    def write_password(self):
+        while True:
+            pw_string = ""
+            characters = int(
+                input(f"how many characters do you want your password to be {self.name}? Pick a number between 8 and 20\n"))
 
-    lowercase = ["abcdefghijklmnopqrstuvwxyz"]
-    uppercase = ["abcdefghijklmnopqrstuvwxyz".upper()]
-    numbers = ["1234567890"]
-    special = ["!@#$%^&*()?<>,./"]
+            if characters < 8 or characters > 20:
+                print("please pick a number between 8 and 20")
+            else:
 
-    selected_criteria = [*lowercase, *uppercase, *special, *numbers]
+                lowercase = ["abcdefghijklmnopqrstuvwxyz"]
+                uppercase = ["abcdefghijklmnopqrstuvwxyz".upper()]
+                numbers = ["1234567890"]
+                special = ["!@#$%^&*()?<>,./"]
 
-    final_criteria = selected_criteria[0] + selected_criteria[1] + selected_criteria[2] + selected_criteria[3]
 
-    pw_string = ""
-    for i in range(characters):
-        pw_string += final_criteria[math.floor(random.random() * len(final_criteria))]
+                selected_criteria = [*lowercase, *uppercase, *special, *numbers]
 
-    return pw_string
+                final_criteria = selected_criteria[0] + selected_criteria[1] + selected_criteria[2] + selected_criteria[3]
+
+                for i in range(characters):
+                    pw_string += final_criteria[math.floor(random.random() * len(final_criteria))]
+
+                return pw_string
+    
 
 
 if __name__ == "__main__":
-    print("hello....")
-    password = write_password()
+    p = Password("parker")
+    password = p.write_password()
     print(password)
