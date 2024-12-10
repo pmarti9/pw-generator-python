27552e02fc5cf8ac177379d6b516cb1b783463ca
Parker Martin <parkerjmartin883@gmail.com>
2024-12-10 00:25:38 -0700

    removed dockerfile and edited composer file

--- src/main.py
+++ src/main.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
     name = input("Name: ")
     p = password.Password(name)
     password = p.write_password()
+    p.save_password_to_redis_store(password)
     print(password)
